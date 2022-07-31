30b480377afcd32c1f44ef3e1e2375541968f54b
dsl2055 <job2019tmm@gmail.com>
2022-07-31 11:02:22 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(5000);
+  const [isBomb, setIsBomb] = useState(false);
+  const handleBomb = () => {
+    setIsBomb(!isBomb);
+    setCount(5000);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isBomb && <div>{`You have ${count / 1000} seconds to run`}</div>}
+      {isBomb && <BombExplode setCount={setCount} count={count} />}
+      <button onClick={handleBomb}>
+        {isBomb ? "Difuse the Bomb" : "Start the bomb"}
+      </button>
     </div>
+  );
+}
+
+function BombExplode({ setCount, count }) {
+  useEffect(() => {
+    let interval = setInterval(() => setCount(count - 1000), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return count < 0 ? (
+    <img
+      src="https://media2.giphy.com/media/HhTXt43pk1I1W/giphy.gif"
+      // width="480"
+      // height="480"
+      // frameBorder="0"
+      // className="giphy-embed"
+      // allowFullScreen
+    />
+  ) : (
+    <img
+      src="https://media2.giphy.com/media/ckCL6oQfuLUuVbB2OH/giphy.gif"
+      // width="480"
+      // height="480"
+      // frameBorder="0"
+      // className="giphy-embed"
+      // allowFullScreen
+    />
   );
 }
 
